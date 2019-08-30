27893def1754b252624acec939af9f8082261b95
Fabio Rämi <fabio.raemi@adfinis-sygroup.ch>
2019-08-30 08:16:54 +0200

    chore(setup): single source metadata
    This commit add `interval_metadata.py` used for single sourcing
    metadata.

---
+++ caluma_interval/interval_metadata.py
@@ -0,0 +1,6 @@
+"""Used to single sourcing metadata about caluma_interval."""
+
+__title__ = "caluma_interval"
+__description__ = "Caluma companion app for periodic usage of forms"
+__version__ = "1.0.0"
+__author__ = "Adfinis SyGroup"
